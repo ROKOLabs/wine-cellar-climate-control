c29e404c93a4aee31e1c8f109fb7ba710a4dcfc1
Ivan Brajković <ivan.brajkovic@icloud.com>
2024-01-30 11:24:26 +0100

    add firebase add firebase config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface Env {
+  VITE_FIREBASE_API_KEY: string;
+  VITE_FIREBASE_AUTH_DOMAIN: string;
+  VITE_FIREBASE_DATABASE_URL: string;
+  VITE_FIREBASE_PROJECT_ID: string;
+  VITE_FIREBASE_STORAGE_BUCKET: string;
+  VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  VITE_FIREBASE_APP_ID: string;
+}
+
+interface ImportMetaEnv extends Env {}
