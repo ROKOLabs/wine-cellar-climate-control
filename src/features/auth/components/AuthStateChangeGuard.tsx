3fc7ead90f2fce9735e900471cad1d0a0b75c4ce
Ivan Brajković <ivan.brajkovic@icloud.com>
2024-02-07 16:55:18 +0100

    prevent routes rendering until auth is initialized

--- src/features/auth/components/AuthStateChangeGuard.tsx
+++ src/features/auth/components/AuthStateChangeGuard.tsx
@@ -1,8 +1,31 @@
-import { Outlet } from 'react-router-dom';
+import { ifElse, juxt, pipe } from 'ramda';
+import { useEffect, useReducer } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
-import { useAuthStateChanged } from 'features/auth/hooks/useAuthStateChanged';
+import { useAuthService } from 'features/auth/hooks/useAuthService';
+import { resetDbApiState } from 'features/db/dbApi';
+import { useAppDispatch } from 'store/hooks';
 
 export const AuthStateChangeGuard = () => {
-  useAuthStateChanged();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const authService = useAuthService();
+  const [isAuthInitialized, initializeAuth] = useReducer(() => true, false);
+
+  useEffect(() => {
+    const goHome = () => navigate('/home');
+    const goLogin = () => navigate('/login');
+    const resetDbState = () => dispatch(resetDbApiState());
+
+    return authService.onAuthStateChanged(
+      juxt([
+        initializeAuth,
+        ifElse(Boolean, goHome, pipe(resetDbState, goLogin)),
+      ]),
+    );
+  }, [authService, dispatch, navigate]);
+
+  // Prevent rendering the children until the auth state is initialized
+  if (!isAuthInitialized) return null;
   return <Outlet />;
 };
