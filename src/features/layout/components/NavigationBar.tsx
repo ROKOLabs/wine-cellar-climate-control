3076feafe4a674a74b54a8e93ae5fdaa3deeccfc
v-sokolic <vanja.sokolic@rokolabs.com>
2024-02-08 14:40:46 +0100

    adapting NavBar for mobile view

--- src/features/layout/components/NavigationBar.tsx
+++ src/features/layout/components/NavigationBar.tsx
@@ -4,6 +4,7 @@ import {
   useMantineColorScheme,
   AppShell,
   Divider,
+  NavLink,
   Avatar,
 } from '@mantine/core';
 import {
@@ -17,11 +18,13 @@ import { useNavigate } from 'react-router-dom';
 
 import { ActionTooltip } from 'components/ActionTooltip';
 import { useSidebarDispatch } from 'features/layout/hooks/useSidebarDispatch';
+import { useIsMobileView } from 'hooks/useIsMobileView';
 
 export const NavigationBar = () => {
   const navigate = useNavigate();
   const toggleSidebar = useSidebarDispatch();
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const isMobile = useIsMobileView();
 
   const goHome = () => navigate('/home');
   const goSettings = () => navigate('/settings');
@@ -32,47 +35,77 @@ export const NavigationBar = () => {
   return (
     <>
       <AppShell.Section>
-        <Stack py="md" align="center">
-          <Avatar tt="uppercase">{userInitials}</Avatar>
-        </Stack>
+        {isMobile ? (
+          <Stack py="md" align="left" pl="sm">
+            <Avatar tt="uppercase">{userInitials}</Avatar>
+          </Stack>
+        ) : (
+          <Stack py="md" align="center">
+            <Avatar tt="uppercase">{userInitials}</Avatar>
+          </Stack>
+        )}
       </AppShell.Section>
 
       <Divider />
 
       <AppShell.Section>
         <Stack py="md" align="center">
-          <ActionTooltip label="Home">
-            <ActionIcon
-              size="xl"
-              radius="md"
-              variant="filled"
-              onClick={juxt([toggleSidebar, goHome])}
-            >
-              <IconHome2 />
-            </ActionIcon>
-          </ActionTooltip>
-          <ActionTooltip label="Settings">
-            <ActionIcon
-              size="xl"
-              radius="md"
-              variant="filled"
-              onClick={juxt([toggleSidebar, goSettings])}
-            >
-              <IconSettings />
-            </ActionIcon>
-          </ActionTooltip>
-          <ActionTooltip
-            label={`Switch to ${colorScheme === 'dark' ? 'light' : 'dark'} mode`}
-          >
-            <ActionIcon
-              size="xl"
-              radius="md"
-              variant="filled"
-              onClick={juxt([toggleSidebar, toggleColorScheme])}
-            >
-              {colorScheme === 'dark' ? <IconSun /> : <IconMoon />}
-            </ActionIcon>
-          </ActionTooltip>
+          {isMobile ? (
+            <>
+              <NavLink
+                label="Home"
+                leftSection={<IconHome2 />}
+                onClick={juxt([toggleSidebar, goHome])}
+              />
+              <NavLink
+                label="Settings"
+                leftSection={<IconSettings />}
+                onClick={juxt([toggleSidebar, goSettings])}
+              />
+              <NavLink
+                label={`Switch to ${colorScheme === 'dark' ? 'light' : 'dark'} mode`}
+                leftSection={
+                  colorScheme === 'dark' ? <IconSun /> : <IconMoon />
+                }
+                onClick={juxt([toggleSidebar, toggleColorScheme])}
+              />
+            </>
+          ) : (
+            <>
+              <ActionTooltip label="Home">
+                <ActionIcon
+                  size="xl"
+                  radius="md"
+                  variant="filled"
+                  onClick={juxt([toggleSidebar, goHome])}
+                >
+                  <IconHome2 />
+                </ActionIcon>
+              </ActionTooltip>
+              <ActionTooltip label="Settings">
+                <ActionIcon
+                  size="xl"
+                  radius="md"
+                  variant="filled"
+                  onClick={juxt([toggleSidebar, goSettings])}
+                >
+                  <IconSettings />
+                </ActionIcon>
+              </ActionTooltip>
+              <ActionTooltip
+                label={`Switch to ${colorScheme === 'dark' ? 'light' : 'dark'} mode`}
+              >
+                <ActionIcon
+                  size="xl"
+                  radius="md"
+                  variant="filled"
+                  onClick={juxt([toggleSidebar, toggleColorScheme])}
+                >
+                  {colorScheme === 'dark' ? <IconSun /> : <IconMoon />}
+                </ActionIcon>
+              </ActionTooltip>
+            </>
+          )}
         </Stack>
       </AppShell.Section>
     </>
