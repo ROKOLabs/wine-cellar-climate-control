c2c864f60456bbc2e0c899602053aca328299009
Ivan Brajković <ivan.brajkovic@icloud.com>
2024-02-15 00:09:23 +0100

    add use render count hook

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,29 +1,13 @@
 import { Container, Paper, Stack } from '@mantine/core';
-import { skipToken } from '@reduxjs/toolkit/query';
-import { useEffect } from 'react';
 
 import { DevTools } from 'components/DevTools';
-import { useAuth } from 'features/auth/hooks/useAuth';
 import { CO2DataDisplay } from 'features/dashboard/components/CO2DataDisplay/CO2DataDisplay';
 import { HumidityDataDisplay } from 'features/dashboard/components/HumidityDataDisplay/HumidityDataDisplay';
 import { TemperatureDataDisplay } from 'features/dashboard/components/TemperatureDataDisplay/TemperatureDataDisplay';
-import {
-  useGetSensorDataQuery,
-  useGetUserDetailsQuery,
-} from 'features/db/dbApi';
+import { useRenderCount } from 'hooks/useRenderCount';
 
 export const Dashboard = () => {
-  const { currentUserUid: userUid } = useAuth();
-  const { data: sensorData } = useGetSensorDataQuery();
-  const { data: userDetails } = useGetUserDetailsQuery(userUid ?? skipToken);
-
-  useEffect(() => {
-    console.log(sensorData);
-  }, [sensorData]);
-
-  useEffect(() => {
-    console.log(userDetails);
-  }, [userDetails]);
+  useRenderCount('Dashboard');
 
   return (
     <Container size="xl">
