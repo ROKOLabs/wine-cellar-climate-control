b7b81b4b192c839875739c83c947d350087929b2
Tomislav Đurin <tomislav.durin@rokolabs.com>
2024-02-15 15:24:42 +0100

    Delete old sensor components

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,9 +1,6 @@
-import { Container, Paper, Stack } from '@mantine/core';
+import { Container, Stack } from '@mantine/core';
 
 import { DevTools } from 'components/DevTools';
-import { CO2DataDisplay } from 'features/dashboard/components/CO2DataDisplay/CO2DataDisplay';
-import { HumidityDataDisplay } from 'features/dashboard/components/HumidityDataDisplay/HumidityDataDisplay';
-import { TemperatureDataDisplay } from 'features/dashboard/components/TemperatureDataDisplay/TemperatureDataDisplay';
 import { useRenderCount } from 'hooks/useRenderCount';
 
 export const Dashboard = () => {
@@ -13,15 +10,6 @@ export const Dashboard = () => {
     <Container size="xl">
       <Stack gap="xl">
         {process.env.NODE_ENV !== 'production' ? <DevTools /> : null}
-        <Paper withBorder shadow="md" p="xl" radius="md">
-          <TemperatureDataDisplay />
-        </Paper>
-        <Paper withBorder shadow="md" p="xl" radius="md">
-          <HumidityDataDisplay />
-        </Paper>
-        <Paper withBorder shadow="md" p="xl" radius="md">
-          <CO2DataDisplay />
-        </Paper>
       </Stack>
     </Container>
   );
