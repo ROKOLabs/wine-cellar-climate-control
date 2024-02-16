a6dfeab362349cc8693a0b0034e0d6e3affad37c
Ivan Brajković <ivan.brajkovic@icloud.com>
2024-02-16 19:06:54 +0100

    extend button and action icon styles

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,10 +1,30 @@
 import '@mantine/notifications/styles.css';
-import { createTheme, MantineProvider } from '@mantine/core';
+import {
+  createTheme,
+  MantineProvider,
+  ActionIcon,
+  Button,
+} from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 
 const theme = createTheme({
   cursorType: 'pointer',
-  activeClassName: 'Button',
+  components: {
+    Button: Button.extend({
+      styles: {
+        root: {
+          transition: 'background-color 300ms ease-out, color 300ms ease-out',
+        },
+      },
+    }),
+    ActionIcon: ActionIcon.extend({
+      styles: {
+        root: {
+          transition: 'background-color 300ms ease-out, color 300ms ease-out',
+        },
+      },
+    }),
+  },
 });
 
 export const ThemeProvider = (props: { children: React.ReactNode }) => (
