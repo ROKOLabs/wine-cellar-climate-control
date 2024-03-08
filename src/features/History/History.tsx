086743866559657543dc1b475805ad959e6339bd
Tomislav Đurin <tomislav.durin@rokolabs.com>
2024-03-08 09:10:10 +0100

    useState optimization, useEventCallback optimization, error handling

--- src/features/History/History.tsx
+++ src/features/History/History.tsx
@@ -1,32 +1,45 @@
 import { Button, Container, Title } from '@mantine/core';
 import { DateValue } from '@mantine/dates';
-import { useCallback, useState } from 'react';
+import { notifications } from '@mantine/notifications';
+import { useState } from 'react';
 
 import { DateRangePicker } from 'features/History/components/DateRangePicker/DateRangePicker';
 import { useLazyGetSensorDataRangeQuery } from 'features/db/dbApi';
+import { useEventCallback } from 'hooks/useEventCallback';
 
 export const History = () => {
   const [getData, { isLoading }] = useLazyGetSensorDataRangeQuery();
 
   const arduinoId = 0;
 
-  const [from, setFrom] = useState<string>(
-    new Date('2022-01-01').toUTCString(),
+  const [from, setFrom] = useState<string>(() =>
+    new Date('2019-01-01').toUTCString(),
   );
-  const onChangeFrom = useCallback((payload: DateValue) => {
+  const onChangeFrom = useEventCallback((payload: DateValue) => {
     setFrom(payload!.toUTCString());
-  }, []);
+  });
 
-  const [to, setTo] = useState<string>(new Date().toUTCString());
-  const onChangeTo = useCallback((payload: DateValue) => {
+  const [to, setTo] = useState<string>(() => new Date().toUTCString());
+  const onChangeTo = useEventCallback((payload: DateValue) => {
     setTo(payload!.toUTCString());
-  }, []);
+  });
 
   const handleGetData = () =>
     getData({ arduinoId, from, to })
       .unwrap()
       .then(console.log)
-      .catch(console.error);
+      .catch(handleError);
+
+  const handleError = () => {
+    notifications.show({
+      title: 'Oops!',
+      message:
+        'It seems something went wrong on our end. Please try again later',
+      color: 'red',
+      withBorder: true,
+      withCloseButton: false,
+    });
+  };
 
   return (
     <Container size="xl">
