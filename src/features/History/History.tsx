e6cc6d1b579d7b624c95c3fa208b5fc50d827064
Ivan Brajković <ivan.brajkovic@icloud.com>
2024-03-25 16:23:49 +0100

    Refactor History component and DateRangePicker

--- src/features/History/History.tsx
+++ src/features/History/History.tsx
@@ -1,5 +1,4 @@
-import { Container, Title, Group, Button } from '@mantine/core';
-import { DateValue } from '@mantine/dates';
+import { Container, Group, Button } from '@mantine/core';
 import { notifications } from '@mantine/notifications';
 import dayjs from 'dayjs';
 import { useState } from 'react';
@@ -7,63 +6,47 @@ import { useState } from 'react';
 import { DateRangePicker } from 'features/History/components/DateRangePicker/DateRangePicker';
 import { Graph } from 'features/History/components/Graph/Graph';
 import { useLazyGetSensorDataRangeQuery } from 'features/db/dbApi';
-import { useEventCallback } from 'hooks/useEventCallback';
+
+const handleError = () => {
+  notifications.show({
+    title: 'Oops!',
+    message: 'It seems something went wrong on our end. Please try again later',
+    color: 'red',
+    withBorder: true,
+    withCloseButton: false,
+  });
+};
+
+// TODO: Replace with actual arduino id from the store
+const arduinoId = 0;
 
 export const History = () => {
-  const [getData, { isLoading, data }] = useLazyGetSensorDataRangeQuery();
-
-  const arduinoId = 0;
-
-  const [from, setFrom] = useState<string>(() =>
-    dayjs().subtract(1, 'month').toDate().toUTCString(),
-  );
-  const onChangeFrom = useEventCallback((payload: DateValue) => {
-    setFrom(payload!.toUTCString());
-  });
-
+  const [from, setFrom] = useState<string>(() => new Date().toUTCString());
   const [to, setTo] = useState<string>(() => new Date().toUTCString());
-  const onChangeTo = useEventCallback((payload: DateValue) => {
-    setTo(payload!.toUTCString());
-  });
+
+  const [getData, { isLoading, data = [] }] = useLazyGetSensorDataRangeQuery();
 
   const handleGetData = () =>
-    getData({ arduinoId, from, to })
-      .unwrap()
-      .then(console.log)
-      .catch(handleError);
-
-  const handleError = () => {
-    notifications.show({
-      title: 'Oops!',
-      message:
-        'It seems something went wrong on our end. Please try again later',
-      color: 'red',
-      withBorder: true,
-      withCloseButton: false,
-    });
-  };
+    getData({ arduinoId, from, to }).unwrap().catch(handleError);
 
   return (
     <Container size="xl">
-      <Title>History Page</Title>
       <Group align="flex-end" mt="xl">
         <DateRangePicker
-          from={new Date(from)}
-          onChangeFrom={onChangeFrom}
-          to={new Date(to)}
-          onChangeTo={onChangeTo}
+          from={from}
+          to={to}
+          onChangeFrom={setFrom}
+          onChangeTo={setTo}
         />
         <Button loading={isLoading} onClick={handleGetData}>
           Get sensor data
         </Button>
       </Group>
-      {data && (
-        <Graph
-          data={data}
-          from={dayjs(from).format('DD/MM/YYYY')}
-          to={dayjs(to).format('DD/MM/YYYY')}
-        />
-      )}
+      <Graph
+        data={data}
+        from={dayjs(from).format('DD.MM.YYYY')}
+        to={dayjs(to).format('DD.MM.YYYY')}
+      />
     </Container>
   );
 };
